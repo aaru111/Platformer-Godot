b40dea002b7417126d5456ccb53d3797768be33d
aaru111 <aarav12303@gmail.com>
2024-12-09 18:12:02 +0530

    (feat): added an input handler with jump buffer and variable jump heights

--- scripts/player/player_fall_state.gd
+++ scripts/player/player_fall_state.gd
@@ -10,12 +10,14 @@ func enter():
 func physics_update(delta):
 	move(delta, true)
 
-	if object.is_jump_just_pressed() and coyote_timer.time_left > 0:
+	if input.jump_just_pressed and coyote_timer.time_left > 0:
 		coyote_timer.stop() 
 		change_state("jump")
 	elif object.is_on_floor():
+		if input.jump_buffer:
+			change_state("jump")
 		coyote_timer.stop()  
-		if object.get_input_x() == 0:
+		if input.x == 0:
 			change_state("idle")
 		else:
 			change_state("run")
--- scripts/player/player_jump_state.gd
+++ scripts/player/player_jump_state.gd
@@ -1,13 +1,22 @@
 extends PlayerBaseState
 
 var jump_sp = -400;
+var var_jump_height
 
 func enter():
 	play("jump")
 	object.velocity.y = jump_sp
+	object.velocity.x += input.x * Player.MAX_SPEED
+	var_jump_height = false
+	input.jump_buffer = false
 	
 func physics_update(delta):
 	move(delta, true)
+	
+	if not var_jump_height and not input.jump_pressed:
+		var_jump_height = true
+		if object.velocity.y <= 0:
+			object.velocity.y /= 2
 	
 	if object.velocity.y >= 0:
 		change_state("fall")
